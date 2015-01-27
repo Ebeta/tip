dc5af4ab76c211721b41ccd7728f779765e1f1b4
Eric Betancourt <eric_betancourt@outlook.com>
2015-01-27 18:38:34 -0500

    this is my tip calculator

---
+++ tip.rb
@@ -0,0 +1,23 @@
+
+
+# bill = 40
+# tip_percent = 0.20
+# num_people = 3
+
+# tip = bill * tip_percent
+# total = bill + tip
+# my_share = total / num_people
+# puts my_share
+
+
+# age = 12
+# puts "I'm #{age} years old"
+
+# a = [1,2,3]
+# a += [4,5,6]
+# p a
+
+b = nil
+b ||= 1 # same as "b = b || 1"
+puts b
+
